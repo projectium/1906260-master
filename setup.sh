ded39543f95a1cec9a66c6a78a4e74ba87b9c8d7
Chyld Medford <chyld.medford@gmail.com>
2019-06-30 16:26:21 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 echo "master clone for projectium organization"
 
-git clone https://github.com/projectium/2019-06-25-001-hello-world
-git clone https://github.com/projectium/2019-06-25-002-student-grades
-git clone https://github.com/projectium/2019-06-26-001-master
-git clone https://github.com/projectium/2019-06-26-002-advanced-javascript
-git clone https://github.com/projectium/2019-06-27-001-javascript-project-template
+git clone https://github.com/projectium/1906251-student-grades-tf
+git clone https://github.com/projectium/1906260-master
+git clone https://github.com/projectium/1906261-experimental-js
+git clone https://github.com/projectium/1906270-js-project-template
+git clone https://github.com/projectium/1906250-js-user-api
